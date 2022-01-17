0d0c686a11145ed0efffa78ee97b652cc32b6d34
Gusde Widnyana <widnyana.bagus@gmail.com>
2022-01-17 11:50:33 +0800

    Update Header

--- layout/header.php
+++ layout/header.php
@@ -8,4 +8,4 @@
     <link rel="stylesheet" href="../public/custom.css">
     <link rel="stylesheet" href="../public/styles.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-  
+   
