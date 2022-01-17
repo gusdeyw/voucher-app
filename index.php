9ea42e2011f17b17addce4ae75c8298c42b0a92e
Gusde Widnyana <widnyana.bagus@gmail.com>
2022-01-17 10:00:17 +0800

    V1.1
    Change Picture database

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "layout/header.php";
+?>
+<title>Bali Coffee Club Digital</title>
+</head>
+<body>
+    
+<!-- Here to place Content  -->
+
+
+
+<!-- Here is the end of the Content -->
+
+<!-- Place Single Javascript this page only script here-->
+
+
+<?php
+include "layout/footer.php";
+?>
